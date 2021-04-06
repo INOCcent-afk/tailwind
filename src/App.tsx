d05da5bb4b5e36d087df4ee0c255677514f34f77
Dave Inoc <michael828inoc@gmail.com>
2021-04-06 20:27:55 +0800

    Added Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,12 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 const App: React.FC = () => {
-  // const [weather, setWeather] = useState(null);
-  // const [weatherTwo, setWeatherTwo] = useState(null);
-  // const [inputWeather, setInputWeather] = useState("");
-
-  // const changeInput = (e: ChangeEvent<HTMLInputElement>) => {
-  //   setInputWeather(e.target.value);
-  // };
-  // const onSubmit = (e: React.FormEvent): void => {
-  //   e.preventDefault();
-  //   axios
-  //     .get(
-  //       `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API}&q=${inputWeather}&aqi=no`
-  //     )
-  //     .then((response) => {
-  //       setWeather(response.data.location.localtime);
-  //       setWeatherTwo(response.data.location.name);
-  //     })
-  //     .catch((err) => console.log(err));
-
-  //   setInputWeather("");
-  // };
-
   return (
-    <div className="App">
-      {/* <h1>{weatherTwo}</h1>
-      <p>{weather}</p>
-
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="location"
-          placeholder="Location"
-          value={inputWeather}
-          onChange={changeInput}
-        />
-        <button>SEARCH LOCATION</button>
-      </form> */}
-      <h1 className="text-red-500">YOOOOOO</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 };
 
--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Dropdown = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>DROPDOWN</h1>
+    </div>
+  );
 };
 
 export default Dropdown;
